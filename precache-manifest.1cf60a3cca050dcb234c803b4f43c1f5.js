a10e4359b22d9060823e3c2d9db47399759db7e5
vanBroekhoven <timvanbroekhoven@outlook.com>
2021-05-07 15:19:20 +0200

    Updates

---
+++ precache-manifest.1cf60a3cca050dcb234c803b4f43c1f5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a7c2e81fdc0ef99ab09307b4ee39e65d",
+    "url": "./index.html"
+  },
+  {
+    "revision": "91b7f8da4607884cbbf0",
+    "url": "./static/css/2.49ecd109.chunk.css"
+  },
+  {
+    "revision": "ffa48154bfb9a282250c",
+    "url": "./static/css/main.ba9e339f.chunk.css"
+  },
+  {
+    "revision": "91b7f8da4607884cbbf0",
+    "url": "./static/js/2.15ec8bbf.chunk.js"
+  },
+  {
+    "revision": "efc0bc89bcf702a3aa1127d8ad15fc2e",
+    "url": "./static/js/2.15ec8bbf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ffa48154bfb9a282250c",
+    "url": "./static/js/main.ad5a277a.chunk.js"
+  },
+  {
+    "revision": "1d6896b13d1316b5acb7",
+    "url": "./static/js/runtime-main.2b4c0fc3.js"
+  }
+]);
